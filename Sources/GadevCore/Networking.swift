ca6029ac38be49e5f965373613efcd13ccb609f6
mozaa <mry.nsth@gmail.com>
2020-10-01 16:53:58 +0700

    Thêm API Networking

---
+++ Sources/GadevCore/Networking.swift
@@ -0,0 +1,83 @@
+//
+//  Networking.swift
+//  GadevCore
+//
+//  Created by Nguyễn Ý on 9/25/20.
+//
+
+import Foundation
+
+protocol NetworkSession {
+    func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void )
+    
+    func post(with request: URLRequest, completionHandler: @escaping (Data?, Error?) -> Void)
+}
+
+
+extension URLSession: NetworkSession {
+    func post(with request: URLRequest, completionHandler: @escaping (Data?, Error?) -> Void) {
+        let task = dataTask(with: request) { data, _, error in
+            completionHandler(data, error)
+        }
+        task.resume()
+    }
+    
+    func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+        let task = dataTask(with: url) { data, _, error in
+            completionHandler(data, error)
+        }
+        task.resume()
+    }
+}
+
+extension GadevCore {
+    
+    public class Networking {
+        
+        public enum NetworkResult<Value> {
+            case success(Value)
+            case failure(Error?)
+        }
+        
+        /// Responsible for handling all networking calls
+        /// - Warning: Must create before using any public APIs
+        public class Manager {
+            public init() {}
+            
+            internal var session: NetworkSession = URLSession.shared
+            
+            /// Call to the live internet to retrieve Data from a specific location
+            /// - Parameters:
+            ///   - url: The location you wish to fetch data from
+            ///   - completionHandler: Return a result object which signifies the status of the request
+            public func loadData(from url: URL, completionHandler: @escaping (NetworkResult<Data>)-> Void) {
+                session.get(from: url) { data, error in
+                    let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
+                    completionHandler(result)
+                }
+            }
+            
+            /// Calls to the live internet to send data to a specific location
+            /// - Parameters:
+            ///   - url: The location you wish to send data to
+            ///   - body: The object you wish to send over the network
+            ///   - completionHandler: Returns a result object which signifies the status of the request
+            public func sendData<I: Codable> (to url: URL, body: I, completionHandler: @escaping (NetworkResult<Data>) -> Void ) {
+                var request = URLRequest(url: url)
+                do {
+                    let httpBody = try JSONEncoder().encode(body)
+                    request.httpBody = httpBody
+                    request.httpMethod = "POST"
+                    session.post(with: request) { (data, error) in
+                        let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
+                        completionHandler(result)
+                    }
+                } catch let error {
+                    return completionHandler(.failure(error))
+                }
+            }
+        }
+        
+    }
+    
+}
---
+++ Tests/GadevCoreTests/GadevNetworkingTests.swift
@@ -0,0 +1,90 @@
+//
+//  GadevNetworkingTests.swift
+//  GadevCoreTests
+//
+//  Created by Nguyễn Ý on 9/25/20.
+//
+
+import XCTest
+@testable import GadevCore
+
+
+class NetworkSessionMock: NetworkSession {
+    var data: Data?
+    var error: Error?
+    
+    func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+        completionHandler(data, error)
+    }
+    
+    func post(with request: URLRequest, completionHandler: @escaping (Data?, Error?) -> Void) {
+        completionHandler(data, error)
+    }
+}
+
+struct MockData: Codable, Equatable {
+    var id: Int
+    var name: String
+}
+
+final class GadevNetworkingTests: XCTestCase {
+
+    func testLoadDataCall  () {
+        
+        let manager = GadevCore.Networking.Manager()
+        let session = NetworkSessionMock()
+        manager.session = session
+        let expectation = XCTestExpectation(description: "Called for data")
+        let data = Data([0, 1, 0, 1])
+        session.data = data
+        
+        let url = URL(fileURLWithPath: "url")
+        
+        manager.loadData(from: url) { result in
+            expectation.fulfill()
+            switch result {
+            case .success(let returnedData):
+                XCTAssertEqual(data, returnedData, "manager returned unexpected data")
+                
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "error forming error result")
+            }
+        }
+
+        wait(for: [expectation], timeout: 20)
+    }
+    
+    func testSendDataCall () {
+        let session = NetworkSessionMock()
+        let manager = GadevCore.Networking.Manager()
+        let sampleObject = MockData(id: 1, name: "Ken")
+        let data = try? JSONEncoder().encode(sampleObject)
+
+        session.data = data
+        manager.session = session
+        
+        let url = URL(fileURLWithPath: "url")
+        let expectation = XCTestExpectation(description: "Sent data")
+        
+        manager.sendData(to: url, body: sampleObject) { result in
+            expectation.fulfill()
+            switch result {
+            case .success(let returnedData):
+                let returnedObject = try? JSONDecoder().decode(MockData.self, from: returnedData)
+                XCTAssertEqual(returnedObject, sampleObject)
+                break
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "error forming error result")
+            }
+            
+        }
+        
+        wait(for: [expectation], timeout: 5)
+    }
+
+    static var allTests = [
+        ("testLoadDataCall", testLoadDataCall),
+        ("testSendDataCall", testSendDataCall),
+    ]
+    
+}
